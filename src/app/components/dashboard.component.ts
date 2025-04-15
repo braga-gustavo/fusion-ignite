badc41be74e422e0bcae0a07adfe557d0243f400
bragagustavo <gustavo.b.moraes2@gmail.com>
2025-04-15 17:06:56 -0300

    dashboard component implementation

--- src/app/components/dashboard.component.ts
+++ src/app/components/dashboard.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { NgChartsModule } from 'ng2-charts';
+import { KPIS } from '../data/kpis';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, NgChartsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  kpis = KPIS;
+
+  barChartOptions: ChartConfiguration['options'] = {
+    responsive:true
+  }
+
+  barChartType: ChartType = 'bar';
+  barChartData = {
+    labels: KPIS.chartData.labels,
+    datasets: [
+      {
+        data: KPIS.chartData.values,
+        label: 'Vendas (R$)',
+        backgroundColor: '#1976d2',
+      },
+    ],
+  };
 }
---
+++ src/app/data/kpis.ts
@@ -0,0 +1,9 @@
+export const KPIS = {
+  sales : 28490,
+  conversion: 5.4,
+  leads: 203,
+  chartData: {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    values: [12000, 15000, 18000, 14000, 21000]
+  }
+};
